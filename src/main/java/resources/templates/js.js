8938094a92b6f5531f30f756f7e64b4fe6ce39b6
Weverton Oliveira Marinho <wevmetal@gmail.com>
2024-10-04 17:20:10 -0300

    Implementada a validação dos campos da página de login

---
+++ src/main/java/resources/templates/js.js
@@ -0,0 +1,16 @@
+function validarUsuario(){
+    const senha = document.getElementById('senha').value;
+    if(document.getElementById("usuario").value == ""){
+        alert("Por favor, digite o nome de usuário")
+    }else if(document.getElementById("senha").value == ""){
+        alert("Por favor, digite a senha de usuário")
+    }   else if (senha.length < 3 || senha.length > 8) {
+        alert("A senha deve ter entre 3 e 8 caracteres.");
+        return;
+}else{
+    alert("Loguin realizado com sucesso!");
+    window.location.href = "./menu.html";
+} 
+   
+ 
+}
