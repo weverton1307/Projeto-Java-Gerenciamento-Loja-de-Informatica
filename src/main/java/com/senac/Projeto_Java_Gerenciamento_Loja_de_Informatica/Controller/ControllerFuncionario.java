79e41652d6500664aa9369d200def94dc560cef2
Weverton Oliveira Marinho <wevmetal@gmail.com>
2024-11-14 13:37:17 -0300

    Implementada a funcionalidade de cadastrar funcionário

--- src/main/java/com/senac/Projeto_Java_Gerenciamento_Loja_de_Informatica/Controller/ControllerFuncionario.java
+++ src/main/java/com/senac/Projeto_Java_Gerenciamento_Loja_de_Informatica/Controller/ControllerFuncionario.java
@@ -1,22 +1,61 @@
 
 package com.senac.Projeto_Java_Gerenciamento_Loja_de_Informatica.Controller;
 
+import com.senac.Projeto_Java_Gerenciamento_Loja_de_Informatica.Model.Cargo;
+import com.senac.Projeto_Java_Gerenciamento_Loja_de_Informatica.Model.Funcionario;
+import com.senac.Projeto_Java_Gerenciamento_Loja_de_Informatica.Model.Usuario;
+import com.senac.Projeto_Java_Gerenciamento_Loja_de_Informatica.Service.ServiceCargo;
 import com.senac.Projeto_Java_Gerenciamento_Loja_de_Informatica.Service.ServiceFuncionario;
+import com.senac.Projeto_Java_Gerenciamento_Loja_de_Informatica.Service.ServiceUsuario;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ControllerFuncionario {
      @Autowired
    ServiceFuncionario serviceFuncionario;
+     @Autowired
+     ServiceCargo serviceCargo;
+     @Autowired
+     ServiceUsuario serviceUsuario;
      
       @GetMapping("/funcionarios")
-    public String inicio() {
-
+    public String inicio(Model model) {
+         model.addAttribute("funcionario", new Funcionario());
+          model.addAttribute("cargo", new Cargo());
+           model.addAttribute("usuario", new Usuario());
         return "funcionarios";
     }
+    
+    
+   @PostMapping("/funcionarios")
+public String processarFormulario(Model model, @RequestBody Funcionario funcionario) {
+     if (serviceCargo == null) {
+        System.out.println("serviceCargo está nulo!");
+        return "erro";
+    }
+        Cargo cargoEncontrado = null;
+        List<Cargo> listaCargo = serviceCargo.listarCargo();
+        for(Cargo c: listaCargo){
+            if(c.getNome().equalsIgnoreCase(funcionario.getCargo().getNome())){
+                cargoEncontrado = c;
+            }
+        }
+        System.out.println("nome "+cargoEncontrado.getNome());
+        Usuario usuario = serviceUsuario.criarUsuario(funcionario.getUsuario());
+        funcionario.setCargo(cargoEncontrado);
+        funcionario.setUsuario(usuario);
+        serviceFuncionario.criarFuncionario(funcionario);
+ 
+    return "funcionarios";
+}
+
 }
--- src/main/resources/static/js/js.js
+++ src/main/resources/static/js/js.js
@@ -1,8 +1,47 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let nomeUsuario = document.getElementById("boasVindasNome").value;
     let tipoUsuario = document.getElementById("boasVindasTipo").value;
 
     if (nomeUsuario && tipoUsuario) {
         alert("Olá, " + nomeUsuario + "! Sua permissão é de " + tipoUsuario + ". Seja bem-vindo!");
     }
+});
+
+$(document).ready(function () {
+    $("#salvarFuncionario").click(function (event) {
+        event.preventDefault();
+        const formData = {
+            nome: $("#nomeFuncionario").val().trim(),
+            endereco: $("#endereço").val().trim(),
+            cpf: $("#cpf").val().trim(),
+            telefone: $("#telefone").val(),
+            email: $("#email").val(),
+            cargo: {
+                nome: $("#cargoFuncionario").val(),
+            },
+            usuario: {
+                login: $("#nomeUsuario").val(),
+                senha: $("#senha-funcionario").val(),
+                tipoUsuario: $("#cargoFuncionario").val()
+            },
+            id: $("#funcionarioId").val() ? parseInt($("#funcionarioId").val()) : null
+        };
+
+
+        console.log("Form Data:", formData);
+
+        $.ajax({
+            type: "POST",
+            url: "/funcionarios",
+            contentType: "application/json",
+            data: JSON.stringify(formData),
+            success: function (response) {
+                alert("Funcionário cadastrado com sucesso!");
+                window.location.href = "/funcionarios";
+            },
+            error: function (xhr, status, error) {
+                alert("Ocorreu um erro: " + xhr.responseText);
+            }
+        });
+    });
 });
